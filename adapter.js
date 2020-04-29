5e1dd6164781fa31a19d0584ec8f09770a5cf760
apporoad <apporoadina@163.com>
2020-04-29 15:57:08 +0800

    rmilink

--- adapter.js
+++ adapter.js
@@ -1,12 +1,8 @@
-require('ilink.js').reg(module,'LiSA.sync')
+//require('ilink.js').reg(module,'LiSA.sync')
 
-exports.getId= D=>{
-    return null
-}
-exports.syncReader= D=>{
-    return null
-}
-exports.reader = D=>{
-    return Promise.resolve(null)
-}
-exports.writer = (D,data)=>{}
+var adapter = require('lisa.sync.fileadapter')
+
+exports.getId= adapter.getId
+exports.syncReader= adapter.syncReader
+exports.reader = adapter.reader
+exports.writer =  adapter.writer
